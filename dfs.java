bbabf5b44ae4e77e046ceedb1d6a7ba25337dcdb
aadya2775 <aadya2775@gmail.com>
2025-05-23 01:23:12 +0530

    dfs.java

--- dfs.java
+++ dfs.java
@@ -1,4 +1,26 @@
 import java.util.*;
 public class dfs {
-    
+    public static void sol(int arr[][],int s,int n,boolean vis[]){
+        vis[s]=true;
+        System.out.println(s);
+        for(int i=0;i<n;i++){
+         if(arr[s][i]== 1 && !vis[i]){
+            sol(arr,i,n,vis);
+         }
+        }
+    }
+ public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        int n =sc.nextInt();
+        int arr[][]=new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        int s =sc.nextInt();
+        boolean[]vis=new boolean[n];
+        sol(arr,s,n,vis);
+        sc.close();
+    }
 }
